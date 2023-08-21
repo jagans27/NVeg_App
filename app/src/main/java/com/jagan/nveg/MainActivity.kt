7a70fb9c0845f655294156e3665042bf15747424
jagan0227 <j2a7g0a2n2003@gmail.com>
2023-08-21 20:25:50 +0530

    commit 1

--- app/src/main/java/com/jagan/nveg/MainActivity.kt
+++ app/src/main/java/com/jagan/nveg/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.jagan.nveg.dashboard.ItemDashboard
-import com.jagan.nveg.detailspage.AboutPage
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
